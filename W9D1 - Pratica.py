4be3e6663f94692bb6db8debe3c0b7ee065acdf5
biagio-analyst <cryfchrb7g@privaterelay.appleid.com>
2026-04-24 17:45:40 +0200

    Add files via upload

---
+++ W9D1 - Pratica.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+studenti= 25
+
+
+# In[3]:
+
+
+print (studenti)
+
+
+# In[5]:
+
+
+nuovi_studenti = 3
+
+
+# In[9]:
+
+
+totale_studenti = studenti + nuovi_studenti
+
+
+# In[10]:
+
+
+print(totale_studenti)
+
+
+# In[11]:
+
+
+nome_scuola='Epicode'
+
+
+# In[12]:
+
+
+print(nome_scuola)
+
+
+# In[14]:
+
+
+x=10
+
+
+# In[17]:
+
+
+y=x+2
+
+
+# In[18]:
+
+
+z=y*3
+
+
+# In[19]:
+
+
+print(y)
+
+
+# In[20]:
+
+
+print(z)
+
+
+# In[21]:
+
+
+str1 = "Windows"
+str2 = "Excel"
+str3 = "Powerpoint"
+str4 = "Word"
+
+
+# In[23]:
+
+
+print (len(str1))
+
+
+# In[24]:
+
+
+print (len(str2))
+
+
+# In[25]:
+
+
+print (len(str3))
+
+
+# In[26]:
+
+
+print (len(str4))
+
+
+# In[27]:
+
+
+secondi=365*24*60*60
+
+
+# In[28]:
+
+
+print(secondi)
+
+
+# In[29]:
+
+
+my_string = "I am studying Python"
+
+
+# In[32]:
+
+
+maiuscolo=my_string.upper()
+
+
+# In[33]:
+
+
+print(maiuscolo)
+
+
+# In[34]:
+
+
+minuscolo=my_string.lower()
+
+
+# In[35]:
+
+
+print(minuscolo)
+
+
+# In[36]:
+
+
+nuovo_testo = my_string.replace("Python", "a lot")
+
+
+# In[37]:
+
+
+print(nuovo_testo)
+
+
+# In[38]:
+
+
+new=my_string.strip()
+
+
+# In[39]:
+
+
+print(new)
+
+
+# In[40]:
+
+
+new2=my_string.split()
+
+
+# In[41]:
+
+
+print(new2)
+
+
+# In[42]:
+
+
+# con il metodo strip non cambia nulla, mentre con il metodo split ti divide ogni singola parola nella frase.
+
+
+# In[43]:
+
+
+studenti = ["Alex", "Bob", "Cindy", "Dan", "Emma", "Faith", "Grace", "Henry"]
+
+
+# In[44]:
+
+
+corsi = ["Cybersecurity", "Data Analyst", "Backend", "Frontend", "Data Analyst", "Backend"]
+
+
+# In[46]:
+
+
+corsi = []
+
+corsi.append("Data Analyst")   # Emma
+corsi.append("Backend")        # Faith
+corsi.append("Frontend")       # Grace
+corsi.append("Cybersecurity")  # Henry
+
+
+# In[47]:
+
+
+print(corsi)
+
+
+# In[ ]:
+
+
+
+
